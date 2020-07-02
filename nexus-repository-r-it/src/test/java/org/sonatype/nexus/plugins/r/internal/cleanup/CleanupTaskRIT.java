ece90eb083bbf97faac22d3d3cacbd8a9e2b11f0
Anatoliy Ornatovskyy <aornatovskyy@sonatype.com>
2020-07-02 07:36:00 +0300

    revert commented tests

--- nexus-repository-r-it/src/test/java/org/sonatype/nexus/plugins/r/internal/cleanup/CleanupTaskRIT.java
+++ nexus-repository-r-it/src/test/java/org/sonatype/nexus/plugins/r/internal/cleanup/CleanupTaskRIT.java
@@ -16,6 +16,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 import org.sonatype.nexus.pax.exam.NexusPaxExamSupport;
 import org.sonatype.nexus.plugins.r.internal.RClient;
@@ -65,11 +66,11 @@ public class CleanupTaskRIT
     deployArtifacts(NAMES);
   }
 
-  //@Test
-  //public void cleanupByLastBlobUpdated() throws Exception {
-  //  assertLastBlobUpdatedComponentsCleanedUp(repository, (long) NAMES.length,
-  //      () -> deployArtifacts(AGRICOLAE_131_TARGZ), 1L);
-  //}
+  @Test
+  public void cleanupByLastBlobUpdated() throws Exception {
+    assertLastBlobUpdatedComponentsCleanedUp(repository, (long) NAMES.length,
+        () -> deployArtifacts(AGRICOLAE_131_TARGZ), 1L);
+  }
 
   @Test
   public void cleanupByLastDownloaded() throws Exception {
@@ -83,17 +84,17 @@ public class CleanupTaskRIT
         () -> deployArtifacts(AGRICOLAE_121_TARGZ, AGRICOLAE_131_TARGZ), 1L);
   }
 
-  //@Test
-  //public void cleanupByLastBlobUpdatedAndLastDownloadedPolicies() throws Exception {
-  //  TestPackage[] versionsOfComponentsToKeep = {AGRICOLAE_121_TARGZ};
-  //
-  //  assertLastBlobUpdatedAndLastDownloadedComponentsCleanUp(
-  //      repository,
-  //      (long) NAMES.length,
-  //      () -> deployArtifacts(AGRICOLAE_131_TARGZ),
-  //      () -> deployArtifacts(versionsOfComponentsToKeep),
-  //      Arrays.stream(versionsOfComponentsToKeep).map(testPackage -> testPackage.filename).toArray(String[]::new));
-  //}
+  @Test
+  public void cleanupByLastBlobUpdatedAndLastDownloadedPolicies() throws Exception {
+    TestPackage[] versionsOfComponentsToKeep = {AGRICOLAE_121_TARGZ};
+
+    assertLastBlobUpdatedAndLastDownloadedComponentsCleanUp(
+        repository,
+        (long) NAMES.length,
+        () -> deployArtifacts(AGRICOLAE_131_TARGZ),
+        () -> deployArtifacts(versionsOfComponentsToKeep),
+        Arrays.stream(versionsOfComponentsToKeep).map(testPackage -> testPackage.filename).toArray(String[]::new));
+  }
 
   @Override
   protected boolean componentMatchesByVersion(final Component component, final String version) {
